4fdf3d2b52054401356b9f987d8636980f7a4103
moneissh <dmoniessh@gmail.com>
2025-05-23 10:32:25 +0530

    Update paypal.js

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox", // required: either "sandbox" or "live"
-  client_id: "dummy-id", // placeholder
-  client_secret: "dummy-secret", // placeholder
+  client_id: "AU7O2wwYIninIOj3NS4uKvQhpgwUpe2CckHoQPApKjwRCym4CRATJXaQqSTqbJ4ruWQUSne0kH2ymand", // placeholder
+  client_secret: "EMwg0OY9YFSv0VKoSHvMD4AiaElySCVldi2aJPjz7BJ9uZcImzXpMUgKW6olVdWvDYVZFPOFNx9CsbIC", // placeholder
 });
 
 module.exports = paypal;
